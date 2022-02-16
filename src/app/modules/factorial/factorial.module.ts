e5f4b2517626e35cfcc58f33f3190846cd52f624
damindda <damindda@gmail.com>
2022-02-16 10:12:15 +0000

    added modules needed

---
+++ src/app/modules/factorial/factorial.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { FactorialRoutingModule } from './factorial-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    FactorialRoutingModule
+  ]
+})
+export class FactorialModule { }
---
+++ src/app/modules/fizzbuzz/fizzbuzz.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { FizzbuzzRoutingModule } from './fizzbuzz-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    FizzbuzzRoutingModule
+  ]
+})
+export class FizzbuzzModule { }
